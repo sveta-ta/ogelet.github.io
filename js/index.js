15029640c8054ea0ff6f22d46ba0434ea5a27376
Ogelet <camarezi@gmail.com>
2019-07-06 22:19:39 -0400

    fix

--- js/index.js
+++ js/index.js
@@ -11,13 +11,14 @@ function init() {
 }
 
 function draw() {
-    ctx.fillStyle = 'rgba(' + (255 - Math.random() * 250) + ',' + (255 - Math.random() * 250) + ',100, 0.7)';
+    ctx.fillStyle = `hsla(${Math.random() * 350}, 94%, 60%, .4)`;
+    ctx.lineWidth = 1;
 
     if (i < 50) {
         for (let j = 0; j < i * 6; j++) { // draw individual dots
             ctx.rotate(Math.PI * 2 / (i * 6));
             ctx.beginPath();
-            ctx.arc(0, i * 35, 25, 0, Math.PI * 2, true);
+            ctx.arc(0, i * 34, 32, 0, Math.PI * 2, true);
             ctx.fill();
         }
     };
